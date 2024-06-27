6d3a79ab34aa6a48d59f4894a3c9b16b17f5d912
Janssen Batista <batistajanssen.dev@gmail.com>
2024-06-26 22:13:15 -0300

    remove comment

--- src/main/java/dev/janssenbatista/literalura/configs/OpenAPIConfiguration.java
+++ src/main/java/dev/janssenbatista/literalura/configs/OpenAPIConfiguration.java
@@ -4,17 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 
-//@OpenAPIDefinition(
-//        info = Info(
-//                contact = Contact(
-//                        name = "Janssen Batista",
-//                        email = "batistajanssen.dev@gmail.com",
-//                        url = "https://github.com/janssenbatista"
-//                ),
-//                title = "JuMarket API",
-//                version = "1.0.0"
-//        )
-//)
 @OpenAPIDefinition(
         info = @Info(
                 title = "LiterAlura",
